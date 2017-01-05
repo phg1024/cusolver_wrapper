f45bec81add30111b09af32ef05641f3489973e1
Peihong Guo <peihongguo@gmail.com>
2017-01-05 17:28:06 -0600

    initial commit

---
+++ cusolver_wrapper.cu
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Sparse>
+
+// CUDA stuff
+#include "cusolverSp.h"
+#include "cusolverSp_LOWLEVEL_PREVIEW.h"
+#include "cuda_runtime.h"
+
+#include "helper_cuda.h"
+#include "helper_cusolver.h"
+
+#include "boost/timer/timer.hpp"
+
+class CUSolverWrapper {
+public:
+
+private:
+
+};
